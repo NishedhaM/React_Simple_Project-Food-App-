a9b6e482bd422e0700a28a69cd646ea4f90d3639
988451835v <Kadnishedha@gmail.com>
2022-06-04 22:38:35 +0530

    first commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,17 @@
+import Pepperoni from "../assets/Pepperoni.jpg";
+import Margherita from "../assets/Margherita.jpg";
+
+
+export const MenuList =[
+    {
+        name:"Pizza",
+        image:Pepperoni,
+        price:300.00
+    },
+    {
+        name: "Margherita Pizza",
+        image: Margherita,
+        price: 11.99,
+      },
+      
+];
